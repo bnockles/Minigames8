d34dc648ec7b950f6fdd2d01edcba066ddc5799b
bnockles <bnockles@gmail.com>
2016-01-14 13:45:01 -0500

    added JFrame methods

--- Minigames8/src/project/directors/Game.java
+++ Minigames8/src/project/directors/Game.java
@@ -1,16 +1,37 @@
 package project.directors;
 
-public class Game {
-	//this is a comment that I addded from the browser
-	public Game() {
-		// TODO Auto-generated constructor stub
-	}
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
-	/**
-	 * @param args
-	 */
+import javax.swing.JFrame;
+
+public class Game extends JFrame{
+
 	public static void main(String[] args) {
-		//new line of code
+		new Game();
 	}
 
+	public Game() {
+		applySettings();//for JFrame-related methods
+		setVisible(true);//makes the frame visible
+		reset();//starts a game
+	}
+
+	private void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void applySettings() {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		//dimensions for the game (can change freely):
+		int width = 1000;
+		int height = 800;
+		//for JFrame
+		setSize(width,height);
+		setLocation((int)(screenSize.getWidth()-width)/2, (int)(screenSize.getHeight()-height)/2);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setUndecorated(true);
+		
+	}
 }
