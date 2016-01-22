b39d3cbd31b8cbdba4cff696747e7b22237e8d71
bnockles <bnockles@gmail.com>
2016-01-22 13:59:24 -0500

    created InstructionScreen

---
+++ Minigames8/src/project/architecture/InstructionScreen.java
@@ -0,0 +1,26 @@
+package project.architecture;
+
+import java.awt.Graphics2D;
+import java.awt.event.KeyListener;
+
+import project.directors.Game;
+import project.directors.Screen;
+
+public class InstructionScreen extends Screen {
+
+	public InstructionScreen(Game game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
+
+	public KeyListener getKeyListener() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void paintScreen(Graphics2D g2) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
