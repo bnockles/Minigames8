ec1fe193da038f3aaae00713646ed05d0f80acf5
Student6 <Student6@10.8.33.140>
2016-02-11 13:50:50 -0500

    HELP

---
+++ GameBoard/src/BasicProperty.java
@@ -0,0 +1,32 @@
+
+public class BasicProperty extends Property {
+		int initialPrice;/** DOES NOT HAVE A SETTER**/
+		int currentPrice;
+		
+		/**
+		 * NEED TO IMPLEMENT OUR CONSTRUCTOR
+		 **/
+		
+		//Getters and Setters
+		public int getInitialPrice(){
+			return this.initialPrice;
+		}
+		
+		public int getCurrentPrice(){
+			return this.currentPrice;
+		}
+		public int setCurrentPrice(int newPrice){
+			this.currentPrice = newPrice;
+		}
+		
+		//Methods required by BUYABLE interface
+		public void buy(){
+			
+		}
+		public void sell(){
+			
+		}
+		public void resetToInitial(){
+			this.currentPrice = this.initialPrice;
+		}
+}
