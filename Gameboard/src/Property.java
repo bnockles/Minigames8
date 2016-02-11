3537e0174539b67736e29b5d637d2111fcaf661b
xperez0834 <xperez0834@bths.edu>
2016-02-11 00:11:47 -0500

    Update Property.java
    wrote constructor

--- Gameboard/src/Property.java
+++ Gameboard/src/Property.java
@@ -1,7 +1,24 @@
 
 public class Property {
-	int intitialPrice;
-	int currentPrice;
 	String name;
 	String description;
+	
+	//Constructor
+	public property(String propertyName, String propertyDesciption){
+		name = propertyName;
+		decription = propertyDesciption;
+		
+	}
+	
+	
+	/** ONLY USED FOR BUYABLE PROPERTIES
+	//Getters and Setters
+	public int getCurrentPrice(){
+		return this.currentPrice;
+	}
+	
+	public void setCurrentPrice(int newPrice){
+		this.currentPrice =  newPrice;
+	}
+	**/
 }
