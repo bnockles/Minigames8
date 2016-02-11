4672002481b0028d7dee5da740152d78a86b4a30
xperez0834 <xperez0834@bths.edu>
2016-02-11 12:20:20 -0500

    Update BasicProperty.java
    Wrote getters and setters for basicProperties

--- Gameboard/src/BasicProperty.java
+++ Gameboard/src/BasicProperty.java
@@ -1,17 +1,28 @@
 
 public class BasicProperty extends Property {
-		int initialPrice;
+		int initialPrice;/** DOES NOT HAVE A SETTER**/
 		int currentPrice;
 		
 		/**
-		 * NEED TO IMPLEMENT OUR OWN CONSTRUCTOR
+		 * NEED TO IMPLEMENT OUR CONSTRUCTOR
 		 **/
 		
+		//Getters and Setters
+		public int getInitialPrice(){
+			return this.initialPrice;
+		}
 		
+		public int getCurrentPrice(){
+			return this.currentPrice;
+		}
+		publiuc int setCurrentPrice(int newPrice){
+			this.currentPrice = newPrice;
+		}
+		
+		//Methods required by BUYABLE interface
 		public void buy(){
 			
 		}
-		
 		public void sell(){
 			
 		}
