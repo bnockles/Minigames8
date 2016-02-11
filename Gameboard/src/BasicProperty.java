fb60e996728556e9abbe623f0b09d52c7e1a7dde
xperez0834 <xperez0834@bths.edu>
2016-02-11 12:04:07 -0500

    Update BasicProperty.java

--- Gameboard/src/BasicProperty.java
+++ Gameboard/src/BasicProperty.java
@@ -2,4 +2,17 @@
 public class BasicProperty extends Property {
 		int initialPrice;
 		int currentPrice;
+		
+		/**
+		 * NEED TO IMPLEMENT OUR OWN CONSTRUCTOR
+		 **/
+		
+		
+		public void buy(){
+			
+		}
+		
+		public void sell(){
+			
+		}
 }
