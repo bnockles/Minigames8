c600b6bda0a7d8d18029282371661561bde683e1
Xavier Perez <xperez084@bths.edu>
2016-02-11 21:43:56 -0500

    Edited generateAllBasicProperties code

--- Gameboard/src/GameBoard.java
+++ Gameboard/src/GameBoard.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 public class GameBoard {
 	//2D arrayList of all property values, we will decide what goes in this array with or peers 
 	ArrayList<ArrayList<Property>> allProperties = new ArrayList<ArrayList<Property>>();
+	ArrayList<ArrayList<String>> allBasicPropertiesInfo = new ArrayList<ArrayList<String>>();
+
 	
 	public static void main(String[] args) {
 		//We will discuss with our peers what the names and descriptions will be
@@ -15,16 +17,15 @@ public class GameBoard {
 	//Generates all Basic Properties at beginning of game
 	/**
 	public void generateallBasicProperties(){
-		for(int i=0;i<allProperties.size();i++){
-			for(int j=0;j<allProperties.size();j++){
-				generateBasicProperty(allProperties.get(i),allProperties.get(i).get(j));
+		for(int i=0;i<allBasicPropertiesInfo.size();i++){
+			for(int j=0;j<allBasicPropertiesInfo.size();j++){
+				generateBasicProperty(allBasicPropertiesInfo.get(i),allBasicPropertiesInfo.get(i).get(j));
 			}
 		}
-		new Property(name,description)
 	}
 	**/
 	public static void generateBasicProperty(String name,String description){
-		printPropertyInfo(new Property(name,description));
+		printPropertyInfo(new BasicProperty(name,description));
 	}
 	private static void printPropertyInfo(Property subject) {
 		System.out.println("You created a basic property with a name of " + 
