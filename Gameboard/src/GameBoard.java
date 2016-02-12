914d3750dd15b2966ab9b64375998d06cf907059
Xavier Perez <xperez084@bths.edu>
2016-02-11 21:32:32 -0500

    renamed method to only work for basic properties

--- Gameboard/src/GameBoard.java
+++ Gameboard/src/GameBoard.java
@@ -6,16 +6,16 @@ public class GameBoard {
 	
 	public static void main(String[] args) {
 		//We will discuss with our peers what the names and descriptions will be
-		generateProperty("Blue1", "Blue1 is the worst property, it's like owning nothing ");
-		generateProperty("Blue2", "Blue2 is the 2nd worst property, it's almost like owning nothing ");
-		generateProperty("Blue3", "Blue3 is the 3rd worst property, it's pretty much like owning nothing ");
+		generateBasicProperty("Blue1", "Blue1 is the worst property, it's like owning nothing ");
+		generateBasicProperty("Blue2", "Blue2 is the 2nd worst property, it's almost like owning nothing ");
+		generateBasicProperty("Blue3", "Blue3 is the 3rd worst property, it's pretty much like owning nothing ");
 		
 	}
-	public static void generateProperty(String name,String description){
+	public static void generateBasicProperty(String name,String description){
 		printPropertyInfo(new Property(name,description));
 	}
 	private static void printPropertyInfo(Property subject) {
-		System.out.println("You created a property with a name of " + 
+		System.out.println("You created a basic property with a name of " + 
 							subject.getName()+
 							" and a description of '"+
 							subject.getDescription()+
