59cccd56c8b612d954269ab4b130df73a1b30ddb
uhhgoat <matyas@fenyves.net>
2016-02-23 21:14:31 -0500

    Created InfoBox class that works with rectangle and text.

--- Minigames8/src/project/directors/InfoBox.java
+++ Minigames8/src/project/directors/InfoBox.java
@@ -3,10 +3,53 @@
  */
 package project.directors;
 
+import java.awt.*;
+
+import javax.swing.*;
+
 /**
  * @author Matyas Fenyves
  *
  */
-public class InfoBox {
-
+public class InfoBox extends JPanel {
+	
+//	public static String info = "hello" ;
+	public static int x ;
+	public static int y ;
+	public static int width ;
+	public static int height ;
+	
+	public InfoBox(int x, int y, int width, int height)
+	{
+		this.x = x ;
+		this.y = y ;
+		this.width = width ;
+		this.height = height ;
+	}
+	
+	public void paint(Graphics g) {
+	    //Dimension d = this.getPreferredSize();
+	    int fontSize = 20;
+	    String info = "hello" ;
+	    g.setFont(new Font("TimesRoman", Font.PLAIN, fontSize));
+	    g.setColor(Color.blue);
+	    g.fillRect(x, y, width, height) ;
+	    g.setColor(Color.red);
+	    
+	    message(g, info) ;
+	  }
+	public void message(Graphics g, String info)
+	{
+	    g.drawString(info, x+25, y+25);
+	}
+	//main is for testing
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.getContentPane().add(new InfoBox(100, 100, 100, 100));
+	
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(200,200);
+		frame.setVisible(true);
+	}
+	
 }
