efaf9bf56b68c702bb1947ebf7f624432fde56c0
Christine <Christine@Lenovo-PC.home>
2016-02-24 22:02:07 -0500

    Updated Timer class

--- Minigames8/src/project/gameDemo/Timer.java
+++ Minigames8/src/project/gameDemo/Timer.java
@@ -7,7 +7,28 @@ public class Timer {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		countdown();
+	}
+	public static void countdown(){
+		countdown(30);
 	}
 
-}
+	public static void countdown(int start){
+		countdown(start, 1);
+	}
+
+	public static void countdown(int start, int step){
+		int num = start;
+		step = Math.abs(step);
+		while (num >= 0)
+		{
+			System.out.print(num + " ");
+			num -= step;
+			long time1 = System.currentTimeMillis();
+			while (System.currentTimeMillis() - time1 < 1000)
+			{
+				//creates a delay
+			}
+		}
+
+	}}
