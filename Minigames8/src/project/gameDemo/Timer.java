0321787544f10b12b73f40d2de6c64ea0d992f79
user1 <user1@pc500>
2016-02-25 00:38:16 -0500

    Updated the Timer class code so that Arik can access its variables

--- Minigames8/src/project/gameDemo/Timer.java
+++ Minigames8/src/project/gameDemo/Timer.java
@@ -1,34 +1,23 @@
 package project.gameDemo;
 
+/* John Hocknell and Christine Yu */
+
 public class Timer {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		countdown();
-	}
-	public static void countdown(){
-		countdown(30);
-	}
-
-	public static void countdown(int start){
-		countdown(start, 1);
-	}
-
-	public static void countdown(int start, int step){
-		int num = start;
-		step = Math.abs(step);
-		while (num >= 0)
-		{
-			System.out.print(num + " ");
-			num -= step;
+	
+	private int seconds;
+	private int interval;
+	
+	public Timer(int seconds, int interval) {
+		this.seconds = seconds;
+		this.interval = interval;	
+		interval = Math.abs(interval); //ensures that the interval is not negative
+		while (seconds >= 0){
+			GameSubTeamScreen.time(seconds);
 			long time1 = System.currentTimeMillis();
-			while (System.currentTimeMillis() - time1 < 1000)
-			{
+			while (System.currentTimeMillis() - time1 < 1000){
 				//creates a delay
 			}
+			seconds -= interval;
 		}
-
-	}}
+	}
+}
