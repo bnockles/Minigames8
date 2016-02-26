14a6bdbbfe19dab5f8489287cc2890d3476445cc
Alexis Xu <axu4945@bths.edu>
2016-02-25 19:14:29 -0500

    changes

--- Gameboard/src/MainMenu.java
+++ Gameboard/src/MainMenu.java
@@ -1,9 +1,11 @@
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
@@ -19,10 +21,20 @@ public class MainMenu extends Screen {
 	}
 
 	public void paintScreen(Graphics2D g2) {
-		g2.setColor(Color.blue);
-		g2.fillRect(0,0,width,height);
-		g2.setColor(Color.white);
-		UtilityMethods.centerTextAtY(g2, "You can select a game on this menu.", width, 50);
+//		BufferedImage bg = new BufferedImage(200, 200, "/images/pokemonDemo.png");
+//		g2.setColor(Color.blue);
+//		g2.fillRect(0,0,width,height);
+//		g2.setColor(Color.white);
+//		UtilityMethods.centerTextAtY(g2, "You can select a game on this menu.", width, 50);
+		File pathToFile = new File("pokemonDemo.png");
+		Image image = null;
+		try {
+			image = ImageIO.read(pathToFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		g2.drawImage(image, 0, 0, 320, 320, null);
 	}
 
 }
