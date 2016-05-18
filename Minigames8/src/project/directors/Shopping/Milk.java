20bc4320e4ed77c1debba7dd7015bde2493c1057
Student8 <Student8@10.8.33.98>
2016-05-18 13:13:44 -0400

    231321

---
+++ Minigames8/src/project/directors/Shopping/Milk.java
@@ -0,0 +1,10 @@
+package project.directors.Shopping;
+
+public class Milk extends Item {
+
+	public Milk(String name, int q, double price, String type) {
+		super(name, q, price, type);
+		// TODO Auto-generated constructor stub
+	}
+
+}
