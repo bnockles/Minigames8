f5605b6d1125cd2510d6fbe3d5bde81a866cbc99
Student8 <Student8@10.8.33.98>
2016-05-18 13:27:54 -0400

    123

--- Minigames8/src/project/directors/Shopping/RunBeverageStore.java
+++ Minigames8/src/project/directors/Shopping/RunBeverageStore.java
@@ -3,6 +3,10 @@ package project.directors.Shopping;
 public class RunBeverageStore {
 	static int POPULATION = 1000;
 	static int DRINKING_AGE = 21;
+	static String[] milk = {"1% fat","2% fat", "lactose-free","whole","fat-free","skim","chocolate","butter","raw"};
+	static String[] juice = {"orange","cherry","grapefruit","grape","lemon","apple","mango"};
+	static String[] beer = {"Guinness","Blue Moon","Heineken","Corona","Budweiser"};
+	static String[] wine = {"Sweet White Wine", "Dry Red Wine", "Sweet Red Wine", "Dry White Wine"}; 
 	/**
 	 * @param args
 	 */
